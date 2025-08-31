ee67a6afa60ad2c4327b81acb49d7ace9ce8fe30
Digvijay-x1 <hackerearthx1@gmail.com>
2025-08-31 23:21:44 +0530

    three

--- backend/src/server.js
+++ backend/src/server.js
@@ -152,13 +152,13 @@ async function testDatabaseConnection() {
   }
 }
 
-
+const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try {
         await testDatabaseConnection() ; 
-        app.listen(ENV.PORT, () => {
-            console.log(`Server running in ${process.env.NODE_ENV} mode on port ${ENV.PORT}`)
+        app.listen(PORT, () => {
+            console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
         });
     } catch (error) {
         console.error('Failed to start server:', error);
